1bff801b7be1929a6e5491f8b62e95756a3a00ac
DevonCrawford <devoncrawfordfilms@gmail.com>
2017-02-01 01:33:38 -0500

    Add files via upload

--- src/TemperatureChange.java
+++ src/TemperatureChange.java
@@ -1,18 +1,27 @@
 import java.util.ArrayList;
 
-public class TemperatureChange extends Change {
+/* This class inherits Change to store data when a temperature (color)
+ * change is detected between images. This class adds temperature data
+ * to the Change class. 
+ * 
+ * by Devon Crawford
+ */
 
+public class TemperatureChange extends Change {
 	private int tempChange;
 	
+	// Initializes change data
 	public TemperatureChange(ArrayList<Image> imgs, int sln, int pln) {
 		super(imgs, sln, pln);
 		increments = tempChange / totalImages;
 	}
 	
+	// Gets temperature change data
 	public int getTemperatureChange() {
 		return tempChange;
 	}
 	
+	// Sets temperature change data
 	public void setTemperatureChange(int tc) {
 		tempChange = tc;
 		increments = tempChange / totalImages;
--- src/Util.java
+++ src/Util.java
@@ -6,27 +6,39 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/* This is my utilities class. It stores functions responsible for
+ * reading/writing data from files, finding/replacing values, and
+ * displaying relevant information.
+ * 
+ * by Devon Crawford
+ */
+
 public class Util {
 
 	// Gets all data from a file, returns as one string
 	public String getData(File file) {
-		String data = "";
-		try {
-			BufferedReader fr = new BufferedReader(new FileReader(file.getAbsolutePath()));
-			String line = fr.readLine();
+		if(file.exists()) {
+			String data = "";
+			try {
+				// Open file for reading
+				BufferedReader fr = new BufferedReader(new FileReader(file.getAbsolutePath()));
+				String line = fr.readLine();
 
-			while (line != null) {
-				data += line;
-				line = fr.readLine();
+				// Reads all lines of file, appending the lines to data string
+				while (line != null) {
+					data += line;
+					line = fr.readLine();
+				}
+				fr.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			fr.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return data;
 		}
-		return data;
+		return null;
 	}
 
 	// Finds a value, given the xmp file, and a key (eg. "Exposure2012")
@@ -34,21 +46,34 @@ public class Util {
 	public String find(File file, String key) {
 		String data = getData(file);
 		String string = "";
+		
+		// Finds start index of value given the key
 		int startLocation = data.indexOf(key) + key.length() + 2;
 		int increment = 0;
+		
+		// Gets start character given the start index
 		char character = data.charAt(startLocation);
 
+		// Reads up until next quotation mark
 		while (character != '"') {
+			// Appends character to string result of data
 			string += character;
+			
+			// Increments index for character
 			increment++;
+			
+			// If location is past end of data, stop search
 			if ((startLocation + increment) >= data.length()) {
 				break;
 			}
+			// Get next character
 			character = data.charAt(startLocation + increment);
 		}
 		return string;
 	}
 
+	// Replaces a value in file from its "key"
+	// Example key: ("Exposure2012")
 	public String replace(File file, String key, double replace) {
 		// Gets file data in string
 		String data = getData(file);
@@ -75,6 +100,7 @@ public class Util {
 		return newData;
 	}
 
+	// Creates a new value after the "preKey" value in file
 	public String createValue(File file, String preKey, String replace, String value) {
 		String data = getData(file);
 
@@ -95,6 +121,7 @@ public class Util {
 		return newFile;
 	}
 
+	// Writes updated string data to file
 	public void writeFile(Image image, String newData) {
 		// Get image data file
 		File newFile = new File(image.getDataPath());
@@ -108,9 +135,25 @@ public class Util {
 			e.printStackTrace();
 		} // true to append
 	}
+	
+	// Get xmp file name from image
+	public String getXMPName(String n) {
+		String xmpName = "";
+		// Iterate each character of name,
+		// append ".xmp" once '.' is found
+		for (int k = 0; k < n.length(); k++) {
+			if (n.charAt(k) != '.') {
+				xmpName += n.charAt(k);
+			} else {
+				xmpName += ".xmp";
+				break;
+			}
+		}
+		return xmpName;
+	}
 
+	// Outputs data of all images in list
 	public void displayInfo(ArrayList<Image> images) {
-		
 		System.out.println("\n================ XMP Information ================");
 		
 		for(int i = 0; i < images.size(); i++) {
